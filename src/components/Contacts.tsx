6856970b75ecf82a9d7be342659bf3102200cf98
Wendell Leão <leaowendell@outlook.com>
2024-03-27 01:38:28 -0300

    Fix OpenResumeFile function

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,10 +1,10 @@
-export function OpenResumeFile(language: string){
-    switch (language){
-        case "en":{
+export function OpenResumeFile(language: string) : void {
+    switch (language) {
+        case "en-US": {
             window.open('https://drive.google.com/file/d/17iNB6TWhaF29Y_UWqBEQNQ6-2XbQpr-x/view?usp=sharing', '_blank');
             break;
         }
-        case "pt":{
+        case "pt-BR": {
             window.open('https://drive.google.com/file/d/1XgBwmCBEXai5-b5pKiaNuvoHm289u7sx/view?usp=sharing', '_blank');
             break;
         }
