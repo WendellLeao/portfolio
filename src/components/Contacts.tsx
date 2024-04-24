ca8ba854eee9a8d433c661b58b55e9287a6a231f
Wendell Leão <leaowendell@outlook.com>
2024-04-24 17:39:28 -0300

    Update resume links

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,11 +1,11 @@
 export function OpenResumeFile(language: string) : void {
     switch (language) {
         case "en-US": {
-            window.open('https://drive.google.com/file/d/1glfB3AiVlKzM01WiB7hPcLcuwXPU8Svg/view?usp=sharing', '_blank');
+            window.open('https://drive.google.com/file/d/12elpJn1RErOFg_y_RC_284JjjkILin8z/view?usp=sharing', '_blank');
             break;
         }
         case "pt-BR": {
-            window.open('https://drive.google.com/file/d/12VKlfIf1WjrQZpcownyhVY1IJ-QOfT_1/view?usp=sharing', '_blank');
+            window.open('https://drive.google.com/file/d/14QbsZk6fhu3mRxeWheei1p67pRl7rKH7/view?usp=sharing', '_blank');
             break;
         }
     }
