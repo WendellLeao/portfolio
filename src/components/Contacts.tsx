770e265a45555ed092ffbcfc25df5612d611387b
Wendell Leão <leaowendell@outlook.com>
2025-01-31 13:27:40 -0300

    chore: update resume links

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,11 +1,11 @@
 export function OpenResumeFile(language: string) : void {
     switch (language) {
         case "en-US": {
-            window.open('https://drive.google.com/file/d/15UpvWG-pXGPbOwzTWTQtqYabc5it8VVq/view?usp=sharing', '_blank');
+            window.open('https://wendellleao.github.io/resume/', '_blank');
             break;
         }
         case "pt-BR": {
-            window.open('https://drive.google.com/file/d/15UpvWG-pXGPbOwzTWTQtqYabc5it8VVq/view?usp=sharing', '_blank');
+            window.open('https://wendellleao.github.io/resume/', '_blank');
             break;
         }
     }
