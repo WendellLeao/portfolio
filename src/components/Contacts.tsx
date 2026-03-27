ac035e53a9816716b4b8896de959ae53ecdfabb0
Wendell Leao <leaowendell@outlook.com>
2026-03-27 16:09:40 -0300

    chore: update the resume link

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,11 +1,11 @@
 export function OpenResumeFile(language: string) : void {
     switch (language) {
         case "en-US": {
-            window.open('https://wendell-leao.com/resume/', '_blank');
+            window.open('https://wendellleao.com/resume/', '_blank');
             break;
         }
         case "pt-BR": {
-            window.open('https://wendell-leao.com/resume/', '_blank');
+            window.open('https://wendellleao.com/resume/', '_blank');
             break;
         }
     }
